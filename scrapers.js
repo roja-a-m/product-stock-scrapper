8cc3a36b4025214e43503a3c533b731d4ce16288
a121213 <roja.am@taem.neustar>
2022-08-07 19:46:58 +0530

    fixing heroku deployment issue

--- scrapers.js
+++ scrapers.js
@@ -32,7 +32,12 @@ async function scrapeProteinProduct(url) {
         }
     ]
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        'args' : [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+          ]
+    });
     const page = await browser.newPage();
     await page.goto(url);
 
