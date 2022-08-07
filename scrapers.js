3b8003eff089273a23420a9d0210a7499fb84776
a121213 <roja.am@taem.neustar>
2022-08-07 20:48:07 +0530

    html response added

--- scrapers.js
+++ scrapers.js
@@ -9,23 +9,27 @@ async function scrapeProteinProduct(url) {
     const productsList = [
         {
             id: "butterMilk8",
+            name:"Buttermilk Pack of 8",
             xpath: '//*[@id="ContentPlaceHolder1_Dv_PCatPrdList_IT1_lblCPrdName_1"]',
             selectorID: "#ContentPlaceHolder1_Dv_PCatPrdList_IT1_lblCPrdName_1",
             inStock: false,
         },
         {
             id: "butterMilk30",
+            name:"Buttermilk Pack of 30",
             xpath: '//*[@id="ContentPlaceHolder1_Dv_PCatPrdList_IT0_lblCPrdName_0"]',
             selectorID: "#ContentPlaceHolder1_Dv_PCatPrdList_IT0_lblCPrdName_0",
             inStock: false,
         },
         {
             id: "lassi8",
+            name:"Lassi Pack of 8",
             xpath: '//*[@id="ContentPlaceHolder1_Dv_PCatPrdList_IT3_lblCPrdName_3"]',
             selectorID: "#ContentPlaceHolder1_Dv_PCatPrdList_IT3_lblCPrdName_3",
             inStock: false,
         }, {
             id: "lassi32",
+            name:"Buttermilk Pack of 32",
             xpath: '//*[@id="ContentPlaceHolder1_Dv_PCatPrdList_IT2_lblCPrdName_2"]',
             selectorID: "#ContentPlaceHolder1_Dv_PCatPrdList_IT2_lblCPrdName_2",
             inStock: false,
@@ -102,6 +106,33 @@ async function scrapeProteinProduct(url) {
     browser.close();
 
 
+    let productRows=""
+    productsList.forEach(p=>{
+        productRows+=`<tr style="height: 18px;"><td style="width: 50%; height: 18px;">`+p.name+`</td>
+        <td style="width: 50%; height: 18px;">`+(p.inStock?'Yes':'No')+`</td></tr>`
+    })
+
+    const htmlStr=`<h1 style="color: #5e9ca0;">Welcome to ScrapityCrawl!</h1>
+    <h2 style="color: #2e6c80;">Website scrapped:</h2>
+    <a href="${amulUrl}" >Amul Protein Products</a>
+    <h2 style="color: #2e6c80;">Run frequency :</h2>
+    <p>`+intervalMs/60000+` mins</p>
+    <h2 style="color: #2e6c80;">Last run was at :</h2>
+    <p>`+new Date()+`</p>
+    <h2 style="color: #2e6c80;">Product Status:</h2>
+    <table style="border-collapse: collapse; width: 100%; height: 90px;" border="1">
+    <tbody>
+    <tr style="height: 18px;">
+    <td style="width: 50%; height: 18px;"><strong>Name</strong></td>
+    <td style="width: 50%; height: 18px;"><strong>In stock?</strong></td>
+    </tr>${productRows}
+    </tbody>
+    </table>
+    <p style="color: #2e6c80;">When products are in stock you will receive a mail at aars095@gmail.com</p>
+    <p>&nbsp;</p>`
+
+    return htmlStr;
+
 }
 
 async function navigateTo(page, buttonID) {
@@ -140,23 +171,24 @@ async function isVisible(page, selector) {
 }
 
 
-// scrapeProteinProduct('https://shop.amul.in/WebForms/Web_Dist_Category_PrdList.aspx?DistId=MTExMTExMQ==&PCatId=MQ==&IsDistPrd=VHJ1ZQ==')
-
-
-
-// 43200000
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+let lastresp = "";
+const intervalMs = 3600000;
+const amulUrl= "https://shop.amul.in/WebForms/Web_Dist_Category_PrdList.aspx?DistId=MTExMTExMQ==&PCatId=MQ==&IsDistPrd=VHJ1ZQ==";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.write(lastresp)
+  res.end();
 });
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
   setInterval( async ()=>{
-    await scrapeProteinProduct('https://shop.amul.in/WebForms/Web_Dist_Category_PrdList.aspx?DistId=MTExMTExMQ==&PCatId=MQ==&IsDistPrd=VHJ1ZQ==')
- }, 60000);
+    lastresp = await scrapeProteinProduct(amulUrl)
+ }, intervalMs);
 });
+
+// 3600000
